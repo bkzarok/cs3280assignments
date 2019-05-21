0f5843c7c7985622a5a91215ef34c3b2a1d8e9e2
Kuir Arok <bkzarok@gmail.com>
2019-05-20 18:01:24 -0600

    changes from visualstudio

--- Assignment1form/Assignment1console/Program.cs
+++ Assignment1form/Assignment1console/Program.cs
@@ -4,12 +4,85 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Assignment1console
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            //Create variables
+            string input;
+            int secondNumber;
+            int firstNumber;
+            int sum;
+
+            //Get the numbers
+            Console.Write("Enter First Number: ");
+            input = Console.ReadLine();
+            firstNumber = Convert.ToInt32(input);
+
+           
+            Console.Write("Enter Second Number: ");
+
+            input = Console.ReadLine();
+            secondNumber = Convert.ToInt32(input); // will throw an exception if fails
+
+
+            //Add the numbers
+            sum = firstNumber + secondNumber;
+
+            //Display the total
+            Console.WriteLine($"\n{firstNumber} + {secondNumber} = {sum}" );
+
+            //Subtract the numbers
+            sum = firstNumber  - secondNumber;
+
+            //Display the total
+            Console.WriteLine($"{firstNumber} - {secondNumber} = {sum}");
+
+            //Multiple the numbers
+            sum = firstNumber * secondNumber;
+
+            //Display the total
+            Console.WriteLine($"{firstNumber} * {secondNumber} = {sum}");
+
+            //Divide the numbers
+            sum = firstNumber / secondNumber;
+
+            //Display the total
+            Console.WriteLine($"{firstNumber} / {secondNumber} = {sum}");
+
+            //Add the numbers
+            sum = firstNumber % secondNumber;
+
+            //Display the total
+            Console.WriteLine($"{firstNumber} % {secondNumber} = {sum}\n");
+
+            if (firstNumber > secondNumber)
+            {
+                   Console.WriteLine($"{firstNumber} is not less than {secondNumber}" );
+                Console.WriteLine($"{firstNumber} is greater than {secondNumber}");
+            }
+
+            if (firstNumber < secondNumber)
+            {
+                Console.WriteLine($"{firstNumber} is not greater than {secondNumber}");
+                Console.WriteLine($"{firstNumber} is less than {secondNumber}" );
+            }
+            if (firstNumber != secondNumber)
+            {
+                Console.WriteLine($"{firstNumber} does not equal {secondNumber}");
+            }
+            else
+            {
+                Console.WriteLine($"{firstNumber} equals {secondNumber}");
+            }
+
+            Console.WriteLine("Press any key to close console");
+            Console.ReadKey();
+
         }
     }
 }
--- Assignment1form/Assignment1form/Form1.cs
+++ Assignment1form/Assignment1form/Form1.cs
@@ -16,5 +16,34 @@ namespace Assignment1form
         {
             InitializeComponent();
         }
+
+       
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+            string input = textBox3.Text;
+            result = MessageBox.Show("You typed: " + input, "Message Box 3", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+            this.label1.Text = "the button " + result + " was clicked";
+            this.textBox3.Text = "";
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+            string input = textBox2.Text;
+            result = MessageBox.Show("You typed: " + input, "Message Box 2", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            this.label1.Text = "the button " + result + " was clicked";
+            this.textBox2.Text = "";
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+            string input = textBox1.Text;
+            result = MessageBox.Show("You typed: " + input, "Message Box 1", MessageBoxButtons.RetryCancel, MessageBoxIcon.Asterisk);
+            this.label1.Text = "the button " +result +" was clicked";
+            this.textBox1.Text = "";
+
+        }
     }
 }
