5eda8495a5e508c72424def2c598c57404080d36
Kuir Arok <bkzarok@gmail.com>
2019-06-12 17:17:33 -0600

    first commit of Assignment3 50% complete

---
+++ Assignment3/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Submit_count_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Reset_scores_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void First_student_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void Last_student_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Save_score_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Display_score_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Previouse_student_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Save_name_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Next_student_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
