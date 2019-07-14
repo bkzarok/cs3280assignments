b058da156a2247189e5391d3cf5ea9a4c094ae68
Kuir Arok <bkzarok@gmail.com>
2019-07-14 15:23:41 -0600

    Assignment for 90 percent done I have bugs that I need to talk to the instructor about put overall it's working fine

--- Assignment4/Assignment4/Form1.Designer.cs
+++ Assignment4/Assignment4/Form1.Designer.cs
@@ -38,18 +38,19 @@
             this.button12 = new System.Windows.Forms.Button();
             this.button22 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.button_play_again = new System.Windows.Forms.Button();
-            this.label_player_1 = new System.Windows.Forms.Label();
-            this.label_player_2 = new System.Windows.Forms.Label();
             this.label_ties = new System.Windows.Forms.Label();
+            this.label_player_2 = new System.Windows.Forms.Label();
+            this.label_player_1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.label_game_status = new System.Windows.Forms.Label();
+            this.button_play_again = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button00
             // 
+            this.button00.Enabled = false;
             this.button00.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button00.Location = new System.Drawing.Point(25, 26);
             this.button00.Name = "button00";
@@ -61,6 +62,7 @@
             // 
             // button10
             // 
+            this.button10.Enabled = false;
             this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button10.Location = new System.Drawing.Point(171, 26);
             this.button10.Name = "button10";
@@ -72,6 +74,7 @@
             // 
             // button20
             // 
+            this.button20.Enabled = false;
             this.button20.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button20.Location = new System.Drawing.Point(312, 26);
             this.button20.Name = "button20";
@@ -83,6 +86,7 @@
             // 
             // button01
             // 
+            this.button01.Enabled = false;
             this.button01.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button01.Location = new System.Drawing.Point(25, 140);
             this.button01.Name = "button01";
@@ -94,6 +98,7 @@
             // 
             // button11
             // 
+            this.button11.Enabled = false;
             this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button11.Location = new System.Drawing.Point(171, 140);
             this.button11.Name = "button11";
@@ -105,6 +110,7 @@
             // 
             // button21
             // 
+            this.button21.Enabled = false;
             this.button21.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button21.Location = new System.Drawing.Point(312, 140);
             this.button21.Name = "button21";
@@ -116,6 +122,7 @@
             // 
             // button02
             // 
+            this.button02.Enabled = false;
             this.button02.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button02.Location = new System.Drawing.Point(25, 254);
             this.button02.Name = "button02";
@@ -127,6 +134,7 @@
             // 
             // button12
             // 
+            this.button12.Enabled = false;
             this.button12.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button12.Location = new System.Drawing.Point(171, 254);
             this.button12.Name = "button12";
@@ -138,6 +146,7 @@
             // 
             // button22
             // 
+            this.button22.Enabled = false;
             this.button22.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button22.Location = new System.Drawing.Point(312, 254);
             this.button22.Name = "button22";
@@ -159,34 +168,14 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Stattistics";
             // 
-            // groupBox2
+            // label_ties
             // 
-            this.groupBox2.Controls.Add(this.label_game_status);
-            this.groupBox2.Location = new System.Drawing.Point(481, 270);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(190, 92);
-            this.groupBox2.TabIndex = 10;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Game Status";
-            // 
-            // button_play_again
-            // 
-            this.button_play_again.Location = new System.Drawing.Point(481, 53);
-            this.button_play_again.Name = "button_play_again";
-            this.button_play_again.Size = new System.Drawing.Size(178, 23);
-            this.button_play_again.TabIndex = 11;
-            this.button_play_again.Text = "Play Again";
-            this.button_play_again.UseVisualStyleBackColor = true;
-            this.button_play_again.Click += new System.EventHandler(this.Button_play_again_Click);
-            // 
-            // label_player_1
-            // 
-            this.label_player_1.AutoSize = true;
-            this.label_player_1.Location = new System.Drawing.Point(24, 39);
-            this.label_player_1.Name = "label_player_1";
-            this.label_player_1.Size = new System.Drawing.Size(84, 13);
-            this.label_player_1.TabIndex = 0;
-            this.label_player_1.Text = "Player 1 Wins: 0";
+            this.label_ties.AutoSize = true;
+            this.label_ties.Location = new System.Drawing.Point(24, 111);
+            this.label_ties.Name = "label_ties";
+            this.label_ties.Size = new System.Drawing.Size(39, 13);
+            this.label_ties.TabIndex = 2;
+            this.label_ties.Text = "Ties: 0";
             // 
             // label_player_2
             // 
@@ -197,14 +186,24 @@
             this.label_player_2.TabIndex = 1;
             this.label_player_2.Text = "Player 2 Wins: 0";
             // 
-            // label_ties
+            // label_player_1
             // 
-            this.label_ties.AutoSize = true;
-            this.label_ties.Location = new System.Drawing.Point(24, 111);
-            this.label_ties.Name = "label_ties";
-            this.label_ties.Size = new System.Drawing.Size(39, 13);
-            this.label_ties.TabIndex = 2;
-            this.label_ties.Text = "Ties: 0";
+            this.label_player_1.AutoSize = true;
+            this.label_player_1.Location = new System.Drawing.Point(24, 39);
+            this.label_player_1.Name = "label_player_1";
+            this.label_player_1.Size = new System.Drawing.Size(84, 13);
+            this.label_player_1.TabIndex = 0;
+            this.label_player_1.Text = "Player 1 Wins: 0";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label_game_status);
+            this.groupBox2.Location = new System.Drawing.Point(481, 270);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(190, 92);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Game Status";
             // 
             // label_game_status
             // 
@@ -214,6 +213,16 @@
             this.label_game_status.Size = new System.Drawing.Size(37, 13);
             this.label_game_status.TabIndex = 0;
             this.label_game_status.Text = "Status";
+            // 
+            // button_play_again
+            // 
+            this.button_play_again.Location = new System.Drawing.Point(481, 53);
+            this.button_play_again.Name = "button_play_again";
+            this.button_play_again.Size = new System.Drawing.Size(178, 23);
+            this.button_play_again.TabIndex = 11;
+            this.button_play_again.Text = "Start Game";
+            this.button_play_again.UseVisualStyleBackColor = true;
+            this.button_play_again.Click += new System.EventHandler(this.Button_play_again_Click);
             // 
             // Form1
             // 
