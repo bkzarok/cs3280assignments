76cd1943d4b11eb0dd23fb4778ceaba1854a9793
Kuir Arok <bkzarok@gmail.com>
2019-07-17 23:32:26 -0600

    commit on 7-17-19

---
+++ Assignment5/Assignment5/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Assignment5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.richTextBoxScores = new System.Windows.Forms.RichTextBox();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.BackColor = System.Drawing.Color.Green;
+            this.buttonStart.ForeColor = System.Drawing.Color.DarkBlue;
+            this.buttonStart.Location = new System.Drawing.Point(85, 81);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(164, 41);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = false;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.BackColor = System.Drawing.Color.Red;
+            this.buttonExit.Location = new System.Drawing.Point(415, 81);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(164, 41);
+            this.buttonExit.TabIndex = 1;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = false;
+            // 
+            // richTextBoxScores
+            // 
+            this.richTextBoxScores.Location = new System.Drawing.Point(85, 187);
+            this.richTextBoxScores.Name = "richTextBoxScores";
+            this.richTextBoxScores.Size = new System.Drawing.Size(494, 228);
+            this.richTextBoxScores.TabIndex = 2;
+            this.richTextBoxScores.Text = "";
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.BackColor = System.Drawing.Color.Yellow;
+            this.buttonLoad.Location = new System.Drawing.Point(617, 231);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(111, 90);
+            this.buttonLoad.TabIndex = 3;
+            this.buttonLoad.Text = "Load Scores";
+            this.buttonLoad.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.richTextBoxScores);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.RichTextBox richTextBoxScores;
+        private System.Windows.Forms.Button buttonLoad;
+    }
+}
+
---
+++ Assignment5/Assignment5/Form2.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Assignment5
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBoxA = new System.Windows.Forms.PictureBox();
+            this.pictureBoxB = new System.Windows.Forms.PictureBox();
+            this.pictureBoxC = new System.Windows.Forms.PictureBox();
+            this.pictureBoxD = new System.Windows.Forms.PictureBox();
+            this.radioButtonA = new System.Windows.Forms.RadioButton();
+            this.radioButtonC = new System.Windows.Forms.RadioButton();
+            this.radioButtonB = new System.Windows.Forms.RadioButton();
+            this.radioButtonD = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxD)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(370, 403);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Next >";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(161, 403);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "<back";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBoxA
+            // 
+            this.pictureBoxA.Location = new System.Drawing.Point(30, 25);
+            this.pictureBoxA.Name = "pictureBoxA";
+            this.pictureBoxA.Size = new System.Drawing.Size(241, 178);
+            this.pictureBoxA.TabIndex = 2;
+            this.pictureBoxA.TabStop = false;
+            // 
+            // pictureBoxB
+            // 
+            this.pictureBoxB.Location = new System.Drawing.Point(370, 25);
+            this.pictureBoxB.Name = "pictureBoxB";
+            this.pictureBoxB.Size = new System.Drawing.Size(245, 178);
+            this.pictureBoxB.TabIndex = 3;
+            this.pictureBoxB.TabStop = false;
+            // 
+            // pictureBoxC
+            // 
+            this.pictureBoxC.Location = new System.Drawing.Point(30, 209);
+            this.pictureBoxC.Name = "pictureBoxC";
+            this.pictureBoxC.Size = new System.Drawing.Size(241, 178);
+            this.pictureBoxC.TabIndex = 4;
+            this.pictureBoxC.TabStop = false;
+            // 
+            // pictureBoxD
+            // 
+            this.pictureBoxD.Location = new System.Drawing.Point(368, 209);
+            this.pictureBoxD.Name = "pictureBoxD";
+            this.pictureBoxD.Size = new System.Drawing.Size(245, 178);
+            this.pictureBoxD.TabIndex = 5;
+            this.pictureBoxD.TabStop = false;
+            // 
+            // radioButtonA
+            // 
+            this.radioButtonA.AutoSize = true;
+            this.radioButtonA.Location = new System.Drawing.Point(277, 99);
+            this.radioButtonA.Name = "radioButtonA";
+            this.radioButtonA.Size = new System.Drawing.Size(85, 17);
+            this.radioButtonA.TabIndex = 6;
+            this.radioButtonA.TabStop = true;
+            this.radioButtonA.Text = "radioButton1";
+            this.radioButtonA.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonC
+            // 
+            this.radioButtonC.AutoSize = true;
+            this.radioButtonC.Location = new System.Drawing.Point(277, 274);
+            this.radioButtonC.Name = "radioButtonC";
+            this.radioButtonC.Size = new System.Drawing.Size(85, 17);
+            this.radioButtonC.TabIndex = 7;
+            this.radioButtonC.TabStop = true;
+            this.radioButtonC.Text = "radioButton2";
+            this.radioButtonC.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonB
+            // 
+            this.radioButtonB.AutoSize = true;
+            this.radioButtonB.Location = new System.Drawing.Point(621, 99);
+            this.radioButtonB.Name = "radioButtonB";
+            this.radioButtonB.Size = new System.Drawing.Size(85, 17);
+            this.radioButtonB.TabIndex = 8;
+            this.radioButtonB.TabStop = true;
+            this.radioButtonB.Text = "radioButton3";
+            this.radioButtonB.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonD
+            // 
+            this.radioButtonD.AutoSize = true;
+            this.radioButtonD.Location = new System.Drawing.Point(621, 274);
+            this.radioButtonD.Name = "radioButtonD";
+            this.radioButtonD.Size = new System.Drawing.Size(85, 17);
+            this.radioButtonD.TabIndex = 9;
+            this.radioButtonD.TabStop = true;
+            this.radioButtonD.Text = "radioButton4";
+            this.radioButtonD.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButtonD);
+            this.Controls.Add(this.radioButtonB);
+            this.Controls.Add(this.radioButtonC);
+            this.Controls.Add(this.radioButtonA);
+            this.Controls.Add(this.pictureBoxD);
+            this.Controls.Add(this.pictureBoxC);
+            this.Controls.Add(this.pictureBoxB);
+            this.Controls.Add(this.pictureBoxA);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxD)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBoxA;
+        private System.Windows.Forms.PictureBox pictureBoxB;
+        private System.Windows.Forms.PictureBox pictureBoxC;
+        private System.Windows.Forms.PictureBox pictureBoxD;
+        private System.Windows.Forms.RadioButton radioButtonA;
+        private System.Windows.Forms.RadioButton radioButtonC;
+        private System.Windows.Forms.RadioButton radioButtonB;
+        private System.Windows.Forms.RadioButton radioButtonD;
+    }
+}
